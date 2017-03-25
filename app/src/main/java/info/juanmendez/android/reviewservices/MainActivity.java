cd99703fbab9cabf90c22af4969364d2d41528c4
Juan Mendez <contact@juanmendez.info>
2017-03-25 13:56:22 -0500

    icepick over toothpick dependency.

--- app/src/main/java/info/juanmendez/android/reviewservices/MainActivity.java
+++ app/src/main/java/info/juanmendez/android/reviewservices/MainActivity.java
@@ -6,11 +6,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import icepick.Icepick;
+import icepick.State;
+
 public class MainActivity extends AppCompatActivity {
 
     private EditText editText;
     private Button button;
     private TextView testResult;
+    @State String fibString;
+
     MainPresenter presenter = new MainPresenter(this);
 
     @Override
@@ -22,12 +27,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Icepick.restoreInstanceState( this, savedInstanceState );
         setContentView(R.layout.activity_main);
 
         editText = (EditText) findViewById(R.id.editText);
         button = (Button) findViewById(R.id.button);
         testResult = (TextView) findViewById(R.id.testResult);
 
+        setResultValue(fibString);
         button.setOnClickListener(view -> {
             String value = editText.getText().toString();
             presenter.doFibonacci( Integer.valueOf(value));
@@ -35,14 +42,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void setResultValue( String s ){
-        testResult.setText( s );
+        testResult.setText( fibString = s );
     }
-
 
     @Override
     protected void onStop() {
         super.onStop();
         presenter.onStop();
+    }
+
+    @Override
+    public void onSaveInstanceState( Bundle outState ){
+        super.onSaveInstanceState(outState);
+        Icepick.saveInstanceState(this, outState);
     }
 
 
