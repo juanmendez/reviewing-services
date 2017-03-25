0273ad58af95bb18562d5c5d8922474b870a822d
Juan Mendez <contact@juanmendez.info>
2017-03-24 22:58:15 -0500

    Success, working with first service

--- app/src/main/java/info/juanmendez/android/reviewservices/MainActivity.java
+++ app/src/main/java/info/juanmendez/android/reviewservices/MainActivity.java
@@ -1,13 +1,34 @@
 package info.juanmendez.android.reviewservices;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+
+import info.juanmendez.android.reviewservices.services.FibonacciService;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.editText);
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(view -> {
+            String value = editText.getText().toString();
+
+            if( !value.isEmpty() ){
+                Intent intent = new Intent(MainActivity.this, FibonacciService.class);
+                intent.putExtra("value", Integer.valueOf(value));
+                startService(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/info/juanmendez/android/reviewservices/services/FibonacciService.java
@@ -0,0 +1,72 @@
+package info.juanmendez.android.reviewservices.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import io.reactivex.Flowable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+
+
+/**
+ * Created by Juan Mendez on 3/24/2017.
+ * www.juanmendez.info
+ * contact@juanmendez.info
+ */
+
+public class FibonacciService extends Service {
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        int febCount = intent.getIntExtra("value", 0);
+
+        /**
+         * This message showed up when I tried instead to run callable in mainThread
+         * 03-24 23:56:53.334 3182-3192/info.juanmendez.android.reviewservices I/art: Background partial concurrent mark sweep GC freed 46(1776B) AllocSpace objects, 64(13MB) LOS objects, 39% free, 5MB/9MB, paused 6.469ms total 58.319ms
+         */
+        if( febCount > 0 ){
+
+            Flowable.fromCallable(() -> {
+                Log.i("FibonacciService", "running in " + Thread.currentThread().getName() );
+                String result = "";
+
+                int[] feb = new int[febCount];
+                feb[0] = 0;
+                feb[1] = 1;
+                for(int i=2; i < febCount; i++){
+                    feb[i] = feb[i-1] + feb[i-2];
+                }
+
+                for(int i=0; i< febCount; i++){
+                    result +=  feb[i] + " ";
+                }
+
+                // See more at: http://www.java2novice.com/java-interview-programs/fibonacci-series/#sthash.Clgk0pQ4.dpuf
+
+                return result;
+            }).subscribeOn(Schedulers.computation())
+              .observeOn(AndroidSchedulers.mainThread())
+              .subscribe(febResult -> {
+                  Log.i("FibonacciService", "running in " + Thread.currentThread().getName() );
+                  Log.i("FibonacciService", "result is " + febResult );
+              });
+        }
+
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
